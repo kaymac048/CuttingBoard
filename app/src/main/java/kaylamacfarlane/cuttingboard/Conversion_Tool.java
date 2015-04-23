413550d458ce42fe9c5d79bffd485cac1d7f6a91
Justine Ciullo <justinec1337@gmail.com>
2015-04-22 20:41:28 -0400

    conversion tool layout

--- app/src/main/java/kaylamacfarlane/cuttingboard/Conversion_Tool.java
+++ app/src/main/java/kaylamacfarlane/cuttingboard/Conversion_Tool.java
@@ -4,14 +4,51 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
-public class Conversion_Tool extends ActionBarActivity {
+public class Conversion_Tool extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+    private static final String[]paths = {"item 1", "item 2", "item 3"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion__tool);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Conversion_Tool.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the third item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
 
