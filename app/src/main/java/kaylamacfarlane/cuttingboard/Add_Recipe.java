b2d71c4460c137834b48ec32a04b8d83f549a902
Bygonaut <Bygonaut@yahoo.com>
2015-05-13 15:22:40 -0400

    Data OutputStream (Attempt)
    Added "store recipe" button with the intentions of saving all the user
    input to the sdcard. WIP.

--- app/src/main/java/kaylamacfarlane/cuttingboard/Add_Recipe.java
+++ app/src/main/java/kaylamacfarlane/cuttingboard/Add_Recipe.java
@@ -1,7 +1,9 @@
 package kaylamacfarlane.cuttingboard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -15,13 +17,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class Add_Recipe extends ActionBarActivity {
     private static final int SELECT_PICTURE = 1;
+    /*private static final String RecipeFile = "RecipeEntry";
+    String FILENAME = "fileTest";
+    FileOutputStream fos = openFileOutput(RecipeFile, Context.MODE_PRIVATE);
+    fos.write(storeRecipe.getBytes());
+    fos.close();*/
+
+
     private String selectedImagePath;
 
     @Override
@@ -30,8 +47,61 @@ public class Add_Recipe extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__recipe);
 
+      //  SharedPreferences recipeStuff = getSharedPreferences(RecipeFile, MODE_PRIVATE);
+      //  SharedPreferences.Editor prefEditor = recipeStuff.edit();
 
-        ((ImageButton) findViewById(R.id.imageButton))
+        final Button button = (Button) findViewById(R.id.ButtonSendRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                File file = new File("/sdcard/storage/sample.txt");
+                String String = "testTest";
+                String FILENAME = "testTest";
+
+                final EditText RecipeTitle = (EditText) findViewById(R.id.Title_editText);
+                String Title = RecipeTitle.getText().toString();
+               // Intent intent = new Intent(Add_Recipe.this,GetText.class);
+
+                final EditText PrepTime = (EditText) findViewById(R.id.PrepTime_editText);
+                String Prep = PrepTime.getText().toString();
+
+                final EditText CookTime= (EditText) findViewById(R.id.cTime_editText);
+                String Cook = CookTime.getText().toString();
+
+                final EditText TotalTime = (EditText) findViewById(R.id.tTime_editText);
+                String Total = TotalTime.getText().toString();
+
+                final EditText Ingredients = (EditText) findViewById(R.id.Ingredients_editText);
+                String Ingr = Ingredients.getText().toString();
+
+                final EditText Directions = (EditText) findViewById(R.id.Directions_editText);
+                String Dir = Directions.getText().toString();
+
+
+                System.out.println(Title + " "+ Prep +" "+ Cook+ " "+ Total+ " "+ Ingr);
+                try (FileOutputStream fop = new FileOutputStream(file)){
+                    if(!file.exists()){
+                        file.createNewFile();
+                    }
+
+                  //  FileOutputStream fos = openFileOutput(file, Context.MODE_WORLD_READABLE);
+                    fop.write(Title.getBytes());
+                    fop.write(Prep.getBytes());
+                    fop.write(Cook.getBytes());
+                    fop.write(Total.getBytes());
+                    fop.write(Ingr.getBytes());
+
+                    System.out.println(file);
+                    fop.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+                ((ImageButton) findViewById(R.id.imageButton))
                 .setOnClickListener(new View.OnClickListener() {
 
                     public void onClick(View arg0) {
@@ -44,6 +114,41 @@ public class Add_Recipe extends ActionBarActivity {
                     }
                 });
     }
+   /* public void storeRecipe(View button)
+    {
+        String FILENAME = "fileTest";
+        String String = "testTest";
+
+        final EditText RecipeTitle = (EditText) findViewById(R.id.Title_editText);
+        String Title = RecipeTitle.getText().toString();
+
+        final EditText PrepTime = (EditText) findViewById(R.id.PrepTime_editText);
+        String Prep = PrepTime.getText().toString();
+
+        final EditText CookTime= (EditText) findViewById(R.id.cTime_editText);
+        String Cook = CookTime.getText().toString();
+
+        final EditText TotalTime = (EditText) findViewById(R.id.tTime_editText);
+        String Total = TotalTime.getText().toString();
+
+        final EditText Ingredients = (EditText) findViewById(R.id.Ingredients_editText);
+        String Ingr = Ingredients.getText().toString();
+
+        System.out.println(Title + " "+ Prep +" "+ Cook+ " "+ Total+ " "+ Ingr);
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,Context.MODE_PRIVATE);
+            fos.write(Title.getBytes());
+            fos.write(Prep.getBytes());
+            fos.write(Cook.getBytes());
+            fos.write(Total.getBytes());
+            fos.write(Ingr.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
     private static int RESULT_LOAD_IMG = 1;
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
--- app/src/main/java/kaylamacfarlane/cuttingboard/Recipe_Box.java
+++ app/src/main/java/kaylamacfarlane/cuttingboard/Recipe_Box.java
@@ -4,6 +4,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Recipe_Box extends ActionBarActivity {
@@ -12,10 +20,34 @@ public class Recipe_Box extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe__box);
+
+        final Button button = (Button) findViewById(R.id.ButtonLoadRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+          //  Add_Recipe AR = new Add_Recipe();
+              //  AR.onCreate(fileList());
+
+                /*File fileStuff = getFileStreamPath(fileTest);
+                if (fileText.exists()) {
+                    BufferedReader in = null;
+                    try {
+                        in = new BufferedReader(new FileReader(fileStuff));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    assert in != null;
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }*/
+            }
+        });
+
     }
 
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_recipe__box, menu);
